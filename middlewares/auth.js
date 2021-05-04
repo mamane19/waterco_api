a02085acb73fb227bc057e53a0b914d563415656
mamane19 <b.moussaama@alustudent.com>
2021-05-04 23:13:01 +0200

    🔨 update db config

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ dotenv.config();
 export function authenticate(req, res, next) {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        jwt.sign(token, process.env.AUTH_KEY);
+        jwt.sign(token, "GcXC4gg2PHdmCDFCVTt0");
         next();
     } catch (err) {
         res.status(401).json({
